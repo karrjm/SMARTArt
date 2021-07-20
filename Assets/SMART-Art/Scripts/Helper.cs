3851bdd3c91f214d0259bcead2a915135b9e2f30
bsrk-1 <61263918+bsrk-1@users.noreply.github.com>
2021-07-20 15:22:26 -0500

    Multi tracking improved

--- Assets/SMART-Art/Scripts/Helper.cs
+++ Assets/SMART-Art/Scripts/Helper.cs
@@ -7,16 +7,35 @@ namespace Scripts
     {
         [SerializeField] private GameObject XButton;
         public GameObject appManager;
+        private int activeTrackers = 0;
+        private bool firstFound = false;
 
         public void Activate()
         {
-            if (appManager.GetComponent<GameManagerScript>().screenSpaceActive)
+            if (appManager.GetComponent<GameManagerScript>().screenSpaceActive || activeTrackers > 0)
             {
                 
             }
             else
             {
                 gameObject.SetActive(true);
+            }
+        }
+
+        public void IncreaseTrackers()
+        {
+            activeTrackers++;
+            if (!firstFound)
+            {
+                firstFound = true;
+            }
+        }
+
+        public void DecreaseTrackers()
+        {
+            if (firstFound)
+            {
+                activeTrackers--;
             }
         }
     }
