cee47ffe01235b0699b26464992b8136be0187a6
Jacob Karr <karrj8285@my.uwstout.edu>
2021-07-21 15:28:58 -0500

    PrefabMenu progress
    - prefab now spawns as child of selection
    - prefab now marks the scene as changed when spawned
    - prefab can now be undone

--- Assets/SMART-Art/Scripts/Editor/PrefabMenu.cs
+++ Assets/SMART-Art/Scripts/Editor/PrefabMenu.cs
@@ -1,38 +1,67 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Scripts.Editor
 {
+#if UNITY_EDITOR
     public static class PrefabMenu
     {
+        #region Utilities
+
         [MenuItem("GameObject/SMART-Art/Utilities", false, 0)]
         private static void DoSomething()
         {
-            
         }
-        
+
+        #endregion
+
+        #region Prefabs
+
         [MenuItem("GameObject/SMART-Art/Prefabs/POI Button", false, 0)]
-        private static void InstantiatePointOfInterestButton()
+        private static void CreatePoiButton()
         {
-            var prefab =
-                AssetDatabase.LoadAssetAtPath("Assets/SMART-Art/Prefabs/POI Button.prefab", typeof(GameObject));
-            PrefabUtility.InstantiatePrefab(prefab);
+            // load prefab
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/POI Button.prefab");
+            
+            // instantiate prefab
+            var go = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            
+            // set prefab as child of selected gameObject in hierarchy
+            if (Selection.activeTransform) go.transform.SetParent(Selection.activeTransform);
+
+            // mark scene as dirty
+            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+
+            // allow undo
+            Undo.RegisterCreatedObjectUndo(go, "Create POI Button");
         }
 
         [MenuItem("GameObject/SMART-Art/Prefabs/Video Panel", false, 0)]
-        private static void InstantiateVideoPanel()
+        private static void CreateVideoPanel()
         {
-            var prefab =
-                AssetDatabase.LoadAssetAtPath("Assets/SMART-Art/Prefabs/Video Panel.prefab", typeof(GameObject));
-            PrefabUtility.InstantiatePrefab(prefab);
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Video Panel.prefab");
+            var go = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            if (Selection.activeTransform) go.transform.SetParent(Selection.activeTransform);
+            
+            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+            
+            Undo.RegisterCreatedObjectUndo(go, "Create Video Panel");
         }
 
         [MenuItem("GameObject/SMART-Art/Prefabs/Card Stack", false, 0)]
-        private static void InstantiateCardStack()
+        private static void CreateCardStack()
         {
-            var prefab =
-                AssetDatabase.LoadAssetAtPath("Assets/SMART-Art/Prefabs/Card Stack.prefab", typeof(GameObject));
-            PrefabUtility.InstantiatePrefab(prefab);
+            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Card Stack.prefab");
+            var go = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            if (Selection.activeTransform) go.transform.SetParent(Selection.activeTransform);
+            
+            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+            
+            Undo.RegisterCreatedObjectUndo(go, "Create Card Stack");
         }
+
+        #endregion
     }
+#endif
 }
