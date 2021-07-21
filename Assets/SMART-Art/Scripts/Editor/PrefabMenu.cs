324a3459e638c227c5ff80eebbd2c6442c1f5ec2
Jacob Karr <karrj8285@my.uwstout.edu>
2021-07-21 15:45:08 -0500

    PrefabMenu cleanup
    less instances of repeated code

--- Assets/SMART-Art/Scripts/Editor/PrefabMenu.cs
+++ Assets/SMART-Art/Scripts/Editor/PrefabMenu.cs
@@ -1,4 +1,5 @@
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,11 +8,14 @@ namespace Scripts.Editor
 #if UNITY_EDITOR
     public static class PrefabMenu
     {
-        #region Utilities
+        #region Helpers
 
-        [MenuItem("GameObject/SMART-Art/Utilities", false, 0)]
-        private static void DoSomething()
+        private static void Create(Object prefab)
         {
+            var creation = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
+            if (Selection.activeTransform) creation.transform.SetParent(Selection.activeTransform);
+            EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
+            Undo.RegisterCreatedObjectUndo(creation, "Create Screenspace Takeaway");
         }
 
         #endregion
@@ -21,47 +25,45 @@ namespace Scripts.Editor
         [MenuItem("GameObject/SMART-Art/Prefabs/POI Button", false, 0)]
         private static void CreatePoiButton()
         {
-            // load prefab
-            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/POI Button.prefab");
-            
-            // instantiate prefab
-            var go = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
-            
-            // set prefab as child of selected gameObject in hierarchy
-            if (Selection.activeTransform) go.transform.SetParent(Selection.activeTransform);
-
-            // mark scene as dirty
-            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
-
-            // allow undo
-            Undo.RegisterCreatedObjectUndo(go, "Create POI Button");
+            var prefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/POI Button.prefab");
+            Create(prefab);
         }
 
         [MenuItem("GameObject/SMART-Art/Prefabs/Video Panel", false, 0)]
         private static void CreateVideoPanel()
         {
-            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Video Panel.prefab");
-            var go = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
-            if (Selection.activeTransform) go.transform.SetParent(Selection.activeTransform);
-            
-            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
-            
-            Undo.RegisterCreatedObjectUndo(go, "Create Video Panel");
+            var prefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Video Panel.prefab");
+            Create(prefab);
         }
 
         [MenuItem("GameObject/SMART-Art/Prefabs/Card Stack", false, 0)]
         private static void CreateCardStack()
         {
-            var prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Card Stack.prefab");
-            var go = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
-            if (Selection.activeTransform) go.transform.SetParent(Selection.activeTransform);
-            
-            UnityEditor.SceneManagement.EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
-            
-            Undo.RegisterCreatedObjectUndo(go, "Create Card Stack");
+            var prefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Card Stack.prefab");
+            Create(prefab);
+        }
+
+        [MenuItem("GameObject/SMART-Art/Prefabs/Card Stack", false, 0)]
+        private static void CreateScreenspaceTakeaway()
+        {
+            var prefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Screenspace Takeaway.prefab");
+            Create(prefab);
+        }
+
+        [MenuItem("GameObject/SMART-Art/Prefabs/Label Canvas", false, 0)]
+        private static void CreateLabelCanvas()
+        {
+            var prefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Label Canvas.prefab");
+            Create(prefab);
         }
 
         #endregion
     }
+    
 #endif
 }
