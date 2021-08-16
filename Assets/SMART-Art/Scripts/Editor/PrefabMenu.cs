bfa3caf787d0c1d6dd859f447eedabe384ea37d6
Jacob Karr <karrj8285@my.uwstout.edu>
2021-08-16 13:34:02 -0500

    prefabs

--- Assets/SMART-Art/Scripts/Editor/PrefabMenu.cs
+++ Assets/SMART-Art/Scripts/Editor/PrefabMenu.cs
@@ -15,61 +15,13 @@ namespace Scripts.Editor
             var creation = PrefabUtility.InstantiatePrefab(prefab) as GameObject;
             if (Selection.activeTransform) creation.transform.SetParent(Selection.activeTransform);
             EditorSceneManager.MarkSceneDirty(SceneManager.GetActiveScene());
-            Undo.RegisterCreatedObjectUndo(creation, "Create Screenspace Takeaway");
+            Undo.RegisterCreatedObjectUndo(creation, "Undo SMART-Art Prefab");
         }
 
         #endregion
 
         #region Prefabs
 
-        [MenuItem("GameObject/SMART-Art/Prefabs/AR Camera", false, 0)]
-        private static void CreateCamera()
-        {
-            var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/ARCamera.prefab");
-            Create(prefab);
-        }
-
-        [MenuItem("GameObject/SMART-Art/Prefabs/POI Button", false, 0)]
-        private static void CreatePoiButton()
-        {
-            var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/POI Button.prefab");
-            Create(prefab);
-        }
-
-        [MenuItem("GameObject/SMART-Art/Prefabs/Video Panel", false, 0)]
-        private static void CreateVideoPanel()
-        {
-            var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Video Panel.prefab");
-            Create(prefab);
-        }
-
-        [MenuItem("GameObject/SMART-Art/Prefabs/Card Stack", false, 0)]
-        private static void CreateCardStack()
-        {
-            var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Card Stack.prefab");
-            Create(prefab);
-        }
-
-        [MenuItem("GameObject/SMART-Art/Prefabs/Card Stack", false, 0)]
-        private static void CreateScreenspaceTakeaway()
-        {
-            var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Screenspace Takeaway.prefab");
-            Create(prefab);
-        }
-
-        [MenuItem("GameObject/SMART-Art/Prefabs/Label Canvas", false, 0)]
-        private static void CreateLabelCanvas()
-        {
-            var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Label Canvas.prefab");
-            Create(prefab);
-        }
-        
         [MenuItem("GameObject/SMART-Art/Prefabs/Image Target", false, 0)]
         private static void CreateImageTarget()
         {
@@ -77,12 +29,44 @@ namespace Scripts.Editor
                 AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/ImageTarget.prefab");
             Create(prefab);
         }
+
+        [MenuItem("GameObject/SMART-Art/Prefabs/Topic Card", false, 0)]
+        private static void CreateTopicCard()
+        {
+            var prefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/TopicCard.prefab");
+            Create(prefab);
+        }
+
+        [MenuItem("GameObject/SMART-Art/Prefabs/Point of Interest (POI)", false, 0)]
+        private static void CreatePoiButton()
+        {
+            var prefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/POI.prefab");
+            Create(prefab);
+        }
         
+        [MenuItem("GameObject/SMART-Art/Prefabs/Card", false, 0)]
+        private static void CreateCard()
+        {
+            var prefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Card.prefab");
+            Create(prefab);
+        }
+
         [MenuItem("GameObject/SMART-Art/Prefabs/Audio Panel", false, 0)]
         private static void CreateAudioPanel()
         {
             var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Audio Panel.prefab");
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/AudioPanel.prefab");
+            Create(prefab);
+        }
+
+        [MenuItem("GameObject/SMART-Art/Prefabs/Video Panel", false, 0)]
+        private static void CreateVideoPanel()
+        {
+            var prefab =
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/VideoPanel.prefab");
             Create(prefab);
         }
 
