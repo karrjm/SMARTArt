b1f4e62447c57cdabd9b1dede88a1db17b71ec41
Jacob Karr <karrj8285@my.uwstout.edu>
2021-08-19 13:48:25 -0500

    cards are now assigned automatically

--- Assets/SMART-Art/Scripts/Stacks/CardStack.cs
+++ Assets/SMART-Art/Scripts/Stacks/CardStack.cs
@@ -1,4 +1,5 @@
-﻿using Scripts.Drag_Controllers;
+﻿using System.Linq;
+using Scripts.Drag_Controllers;
 using Scripts.Utilities;
 using UnityEngine;
 using UnityEngine.UI;
@@ -34,6 +35,8 @@ namespace Scripts.Stacks
 
         private void Awake()
         {
+            cards = GetComponentsInChildren<Transform>();
+            cards = cards.Where(child => child.CompareTag("Card")).ToArray();
             _fader = gameObject.GetComponent<UIFader>();
             _appManager = GameObject.Find("ARCamera");
         }
