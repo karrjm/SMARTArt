25e71445b337a3e93f2d1598194c1c0a0ce4718a
Jacob Karr <karrjm@gmail.com>
2022-06-13 14:56:29 -0500

    new ENGL480 scenes and one fix
    -one scene for cloud and one for device
    -fixed prefab instantiation, simply pointing to wrong location

---
+++ Assets/_SMART-Art/SMART-Art Assets/Scripts/Cloud Reco/SimpleCloudRecoEventHandler.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class SimpleCloudRecoEventHandler : MonoBehaviour
+{
+    private CloudRecoBehaviour mCloudRecoBehaviour;
+    private bool mIsScanning = false;
+    private string mTargetMetadata = "";
+    public ImageTargetBehaviour ImageTargetTemplate;
+
+    // Register cloud reco callbacks
+    private void Awake()
+    {
+        mCloudRecoBehaviour = GetComponent<CloudRecoBehaviour>();
+        mCloudRecoBehaviour.RegisterOnInitializedEventHandler(OnInitialized);
+        mCloudRecoBehaviour.RegisterOnInitErrorEventHandler(OnInitError);
+        mCloudRecoBehaviour.RegisterOnUpdateErrorEventHandler(OnUpdateError);
+        mCloudRecoBehaviour.RegisterOnStateChangedEventHandler(OnStateChanged);
+        mCloudRecoBehaviour.RegisterOnNewSearchResultEventHandler(OnNewSearchResult);
+    }
+    //Unregister cloud reco callbacks when the handler is destroyed
+    private void OnDestroy()
+    {
+        mCloudRecoBehaviour.UnregisterOnInitializedEventHandler(OnInitialized);
+        mCloudRecoBehaviour.UnregisterOnInitErrorEventHandler(OnInitError);
+        mCloudRecoBehaviour.UnregisterOnUpdateErrorEventHandler(OnUpdateError);
+        mCloudRecoBehaviour.UnregisterOnStateChangedEventHandler(OnStateChanged);
+        mCloudRecoBehaviour.UnregisterOnNewSearchResultEventHandler(OnNewSearchResult);
+    }
+
+    private static void OnInitialized(TargetFinder targetFinder)
+    {
+        Debug.Log("Cloud Reco initialized");
+    }
+
+    private static void OnInitError(TargetFinder.InitState initError)
+    {
+        Debug.Log("Cloud Reco init error " + initError.ToString());
+    }
+
+    private static void OnUpdateError(TargetFinder.UpdateState updateError)
+    {
+        Debug.Log("Cloud Reco update error " + updateError.ToString());
+    }
+
+    private void OnStateChanged(bool scanning)
+    {
+        mIsScanning = scanning;
+        if (scanning)
+        {
+            // clear all known trackables
+            var tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+            tracker.GetTargetFinder<ImageTargetFinder>().ClearTrackables(false);
+        }
+    }
+    // Here we handle a cloud target recognition event
+    private void OnNewSearchResult(TargetFinder.TargetSearchResult targetSearchResult)
+    {
+        TargetFinder.CloudRecoSearchResult cloudRecoSearchResult =
+            (TargetFinder.CloudRecoSearchResult)targetSearchResult;
+        // do something with the target metadata
+        mTargetMetadata = cloudRecoSearchResult.MetaData;
+        // stop the target finder (i.e. stop scanning the cloud)
+        mCloudRecoBehaviour.CloudRecoEnabled = false;
+        // Build augmentation based on target 
+        if (ImageTargetTemplate)
+        {
+            // enable the new result with the same ImageTargetBehaviour: 
+            ObjectTracker tracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+            tracker.GetTargetFinder<ImageTargetFinder>().EnableTracking(targetSearchResult, ImageTargetTemplate.gameObject);
+        }
+    }
+
+    private void OnGUI()
+    {
+        // Display current 'scanning' status
+        GUI.Box(new Rect(100, 100, 200, 50), mIsScanning ? "Scanning" : "Not scanning");
+        // Display metadata of latest detected cloud-target
+        GUI.Box(new Rect(100, 200, 200, 50), "Metadata: " + mTargetMetadata);
+        // If not scanning, show button
+        // so that user can restart cloud scanning
+        if (!mIsScanning)
+        {
+            if (GUI.Button(new Rect(100, 300, 200, 50), "Restart Scanning"))
+            {
+                // Restart TargetFinder
+                mCloudRecoBehaviour.CloudRecoEnabled = true;
+            }
+        }
+    }
+}
--- Assets/_SMART-Art/SMART-Art Assets/Scripts/Editor/PrefabMenu.cs
+++ Assets/_SMART-Art/SMART-Art Assets/Scripts/Editor/PrefabMenu.cs
@@ -22,51 +22,51 @@ namespace Scripts.Editor
 
         #region Prefabs
 
-        [MenuItem("GameObject/SMART-Art/Prefabs/Image Target", false, 0)]
+        [MenuItem("GameObject/_SMART-Art/Prefabs/Image Target", false, 0)]
         private static void CreateImageTarget()
         {
             var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/ImageTarget.prefab");
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_SMART-Art/Prefabs/ImageTarget.prefab");
             Create(prefab);
         }
 
-        [MenuItem("GameObject/SMART-Art/Prefabs/Topic Card", false, 0)]
+        [MenuItem("GameObject/_SMART-Art/Prefabs/Topic Card", false, 0)]
         private static void CreateTopicCard()
         {
             var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/TopicCard.prefab");
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_SMART-Art/Prefabs/TopicCard.prefab");
             Create(prefab);
         }
 
-        [MenuItem("GameObject/SMART-Art/Prefabs/Point of Interest (POI)", false, 0)]
+        [MenuItem("GameObject/_SMART-Art/Prefabs/Point of Interest (POI)", false, 0)]
         private static void CreatePoiButton()
         {
             var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/POI.prefab");
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_SMART-Art/Prefabs/POI.prefab");
             Create(prefab);
         }
 
-        [MenuItem("GameObject/SMART-Art/Prefabs/Card", false, 0)]
+        [MenuItem("GameObject/_SMART-Art/Prefabs/Card", false, 0)]
         private static void CreateCard()
         {
             var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/Card.prefab");
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_SMART-Art/Prefabs/Card.prefab");
             Create(prefab);
         }
 
-        [MenuItem("GameObject/SMART-Art/Prefabs/Audio Panel", false, 0)]
+        [MenuItem("GameObject/_SMART-Art/Prefabs/Audio Panel", false, 0)]
         private static void CreateAudioPanel()
         {
             var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/AudioPanel.prefab");
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_SMART-Art/Prefabs/AudioPanel.prefab");
             Create(prefab);
         }
 
-        [MenuItem("GameObject/SMART-Art/Prefabs/Video Panel", false, 0)]
+        [MenuItem("GameObject/_SMART-Art/Prefabs/Video Panel", false, 0)]
         private static void CreateVideoPanel()
         {
             var prefab =
-                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/SMART-Art/Prefabs/VideoPanel.prefab");
+                AssetDatabase.LoadAssetAtPath<GameObject>("Assets/_SMART-Art/Prefabs/VideoPanel.prefab");
             Create(prefab);
         }
 
